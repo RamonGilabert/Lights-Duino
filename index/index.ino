142ff9e8b8481bf0615c6a2c2909fd221121b4c1
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-04-06 00:18:15 +0200

    Improves the code

--- index/index.ino
+++ index/index.ino
@@ -9,25 +9,6 @@ int redLED = 10;
 int greenLED = 11;
 int blueLED = 12;
 
-ISR(TIMER2_OVF_vect) { 
-  TCNT2 = 0;
-  timeout++;
-  
-  if (timeout > 61) {
-    state=1;
-    timeout=0;
-  }
-}
- 
-void init_timer2(void) {
-  TCCR2A |= (1 << WGM21) | (1 << WGM20);   
-  TCCR2B |= 0x07;
-  ASSR |= (0<<AS2);
-  TIMSK2 |= 0x01;
-  TCNT2 = 0;
-  sei();   
-}
-
 void setup() {
   Serial.begin(9600);
 
@@ -37,12 +18,9 @@ void setup() {
   pinMode(blueLED, OUTPUT);
 
   pinMode(0, INPUT);
- 
-  attachInterrupt(0, cleantime, FALLING);
-  init_timer2();
 }
 
-void control(void) {
+void loop() {
   if (Serial.available() == 3) {
     for (int i = 0; i <= 2; i++) {
       information[i] = Serial.read(); 
@@ -52,20 +30,5 @@ void control(void) {
     analogWrite(greenLED, information[1]);
     analogWrite(blueLED, information[2]);
   }
-}
-
-void loop() {
-  switch(state) {
-    case 0:
-      break;
-    case 1:
-      control(); 
-      break;
-  }
-}
- 
-void cleantime() {
-  timeout = 0;
-  state = 0;
 }
 
